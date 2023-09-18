33f8e83e84f73d6629aae798a196fc925321c668
Deni Darmayana <74420243+denidarmayana@users.noreply.github.com>
2023-09-18 16:28:55 +0700

    infaq
    infaq

--- application/views/admin/sumary.php
+++ application/views/admin/sumary.php
@@ -3,6 +3,7 @@ $omset = $this->db->select_sum('paket.amount')->join("paket",'subcribe.paket=pak
 $all_omset = $this->db->select_sum('amount')->get('subcribe')->row();
 $unik_omset = $all_omset->amount-$omset->amount;
 $wd = $this->db->select_sum('fee')->get('widthdrawal')->row();
+$infaq = $this->db->select_sum('amount')->get('infaq')->row();
 ?>
 <div class="row">
 	<div class="col-sm-12 col-12">
@@ -15,6 +16,7 @@ $wd = $this->db->select_sum('fee')->get('widthdrawal')->row();
 				<p class="text-black m-0">Total Kode Unik Omset : <?=number($unik_omset) ?> MBIT</p>
 				<p class="text-black m-0">Total Net Omset : <?=number($omset->amount) ?> MBIT</p>
 				<p class="text-black m-0">Total fee WD : <?=number($wd->fee) ?> MBIT</p>
+				<p class="text-black m-0">Total Infaq : <?=number($infaq->amount) ?> MBIT</p>
 			</div>
 		</div>
 	</div>
