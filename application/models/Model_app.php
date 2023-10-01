d3ba07bc5ffc1f30c79d23441ccc88da3635c65e
Deni Darmayana <74420243+denidarmayana@users.noreply.github.com>
2023-10-01 10:31:39 +0700

    protek
    protek

--- application/models/Model_app.php
+++ application/models/Model_app.php
@@ -79,11 +79,11 @@ class Model_app extends CI_Model
 	public function cekPaket()
 	{
 		$sub = $this->db->select('paket.amount')->order_by('subcribe.id','desc')->join('paket','paket.id=subcribe.paket')->get_where("subcribe",['subcribe.members'=>$this->session->userdata("username")])->row();
-		$limit = $sub->amount *(300/100);
+		$limit = $sub->amount * (300/100);
 		$bns_level = $this->db->select_sum("amount")->get_where("bsn_reff",['receive'=>$this->session->userdata("username")])->row();
 		$roi = $this->db->select_sum("amount")->get_where("roi",['members'=>$this->session->userdata("username")])->row();
 		$penerimaan = $bns_level->amount+$roi->amount;
-		if ($limit >= $penerimaan) {
+		if ($penerimaan >= $limit ) {
 			redirect("package");
 		}else{
 			return TRUE;
