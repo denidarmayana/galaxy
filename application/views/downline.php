c93d157d41037a621d7520a63d673b57240a5c5b
Deni Darmayana <74420243+denidarmayana@users.noreply.github.com>
2023-10-04 11:51:20 +0700

    up
    up

--- application/views/downline.php
+++ application/views/downline.php
@@ -1,27 +1,47 @@
+<?php 
+if (!isset($_GET['users'])) {
+	$liset_downline = $downline;
+}else{
+	$liset_downline = $this->db->get_where("members",['upline'=>$_GET['users']])->result();
+}
+$total_omset = 0;
+foreach ($liset_downline as $keys) {
+	$omsets = $this->db->select('paket.amount')->order_by('subcribe.id','desc')->join('paket','paket.id=subcribe.paket')->get_where("subcribe",['members'=>$keys->username])->row();
+	$total_omset += $omsets->amount;
+}
+?>
 <div class="row">
 	<div class="col-sm-12 col-12 p-md-0">
 		<div class="card">
 			<div class="card-header">
-				<H4 class="card-title">List Downline</H4>
+				<H4 class="card-title">
+					List Downline
+
+					
+				</H4>
+				<span class="float-end text-warning"><?=number($total_omset) ?> MBIT</span>
 			</div>
 			<div class="card-body">
 				<div id="DZ_W_Todo1" class="widget-media dz-scroll">
 					
 					<ul class="timeline">
 						<?php $no=0; 
-						if (!isset($_GET['users'])) {
-							$liset_downline = $downline;
-						}else{
-							$liset_downline = $this->db->get_where("members",['upline'=>$_GET['users']])->result();
-						}
+						$omset_downline=0;
 						foreach ($liset_downline as $key) {$no++;
-							$s = $this->db->select('name')->join('paket','paket.id=subcribe.paket')->get_where("subcribe",['members'=>$key->username])->row();
+							$s = $this->db->select('paket.name')->order_by('subcribe.id','desc')->join('paket','paket.id=subcribe.paket')->get_where("subcribe",['members'=>$key->username])->row();
 							$jb = $this->db->get_where("peringkat",['id'=>$key->position])->row();
 							$lev = $this->db->get_where("members",['upline'=>$key->username])->num_rows();
 							if ($lev > 0) {
 								$btn = '<a href="?users='.$key->username.'" class="btn btn-xs btn-success">'.$lev.' Users</a>';
 							}else{
 								$btn = $lev." Users";
+							}
+							$o_l1 = $this->db->get_where("members",['upline'=>$key->username])->result();
+							$omst = 0;
+							foreach ($o_l1 as $l1) {
+								$om_1 = $this->db->select('paket.amount')->order_by('subcribe.id','desc')->join('paket','paket.id=subcribe.paket')->get_where("subcribe",['members'=>$l1->username])->row();
+								$omst += ($om_1 ? $om_1->amount : 0 );
+								
 							}
 						?>
 						<li>
@@ -30,14 +50,21 @@
 									<img alt="image" width="45" src="<?=base_url() ?>assets/logo.png">
 								</div>
 								<div class="media-body">
-									<h5 class="mb-1"><?=$key->name ?> <span class="float-end text-success"><?=$btn ?>  </span></h5>
+									<h5 class="mb-1"><?=$key->name ?> / <span class="text-warning"><?=number($omst) ?> MBIT</span> <span class="float-end text-success"><?=$btn ?>  </span></h5>
 									<small class="d-block"><?=($s ? $s->name:"No Active Packages") ?> / <?=($key->position == 1 ? $jb->name:"No Position") ?> / <?=$key->created_at ?></small>
 								</div>
 							</div>
 						</li>
-						<?php } ?>
+						<?php $omset_downline +=$omst; } ?>
 					</ul>
 				</div>
+			</div>
+			<div class="card-footer">
+				<h3 class="card-title">
+					Total Personal Turnover
+					<span class="float-end text-warning"><?=number($total_omset+$omset_downline) ?> MBIT</span>
+				</h3>
+				
 			</div>
 		</div>
 	</div>
