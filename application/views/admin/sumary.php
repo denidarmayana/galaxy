2f53512cf4cabfd6ce0a4b584d4f5a30300147a9
Deni Darmayana <74420243+denidarmayana@users.noreply.github.com>
2023-10-09 06:53:19 +0700

    wd

--- application/views/admin/sumary.php
+++ application/views/admin/sumary.php
@@ -3,6 +3,7 @@ $omset = $this->db->select_sum('paket.amount')->join("paket",'subcribe.paket=pak
 $all_omset = $this->db->select_sum('amount')->get_where('subcribe',['status'=>1])->row();
 $unik_omset = $all_omset->amount-$omset->amount;
 $wd = $this->db->select_sum('fee')->get_where('widthdrawal',['status'=>1])->row();
+$net = $this->db->select_sum('net')->get_where('widthdrawal',['status'=>1])->row();
 $infaq = $this->db->select_sum('amount')->get('infaq')->row();
 ?>
 <div class="row">
@@ -16,6 +17,7 @@ $infaq = $this->db->select_sum('amount')->get('infaq')->row();
 				<p class="text-black m-0">Total Kode Unik Omset : <?=number($unik_omset) ?> MBIT</p>
 				<p class="text-black m-0">Total Net Omset : <?=number($omset->amount) ?> MBIT</p>
 				<p class="text-black m-0">Total fee WD : <?=number($wd->fee) ?> MBIT</p>
+				<p class="text-black m-0">Total WD : <?=number($net->net) ?> MBIT</p>
 				<p class="text-black m-0">Total Infaq : <?=number($infaq->amount) ?> MBIT</p>
 			</div>
 		</div>
